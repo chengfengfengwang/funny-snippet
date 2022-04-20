d1bb8fcb1e7a3eb2643d0e8649c86de29d89716b
wangchengfeng <wangchengfeng@xiaoyezi.com>
2022-04-20 19:09:49 +0800

    input only number

--- funny.js
+++ funny.js
@@ -8,3 +8,12 @@ function loadScript(url, callback) {
 loadScript('/qwe', () => {
     console.log('hhhhh')
 })
+
+// 全局input only number
+document.addEventListener('input', function(e) {
+    const target = e.target;
+    const classList = Array.from(target.classList)
+    if (classList.indexOf('input_number') !== -1) {
+        target.value = target.value.replace(/[^\d]/g, '')
+    }
+})
