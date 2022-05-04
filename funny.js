b66d625b4fc4113768f340999773983c51a13144
wangchengfeng <602120734@qq.com>
2022-05-04 14:24:34 +0800

    时间点差

--- funny.js
+++ funny.js
@@ -28,3 +28,39 @@ Math.floor(Math.random() * (13 - 3)) + 3
 function random(min, max) {
     return (Math.floor(Math.random() * (max - min + 1)) + min)
 }
+
+// 记录函数调用次数
+function sayHi(name) {
+    sayHi.count++;
+    console.log(`hello ${name}`)
+}
+sayHi.count = 0 // 初始值
+sayHi('a');
+sayHi('b'); // count为2
+// 函数表达式
+let sayHello = function fn (who) { // fn只在函数内部可见
+    if (who) {
+        console.log(`hello ${who}`);
+    } else {
+        fn('default name')
+    }
+}
+let welcome = sayHello;
+sayHello = null;
+welcome()
+
+//计算两个时间点相差几时几分，时间点最多跨今明两天
+function getDiff(h1, m1, h2, m2) {
+    if (h1 <= h2) {
+        const diff = getMinutesToday(h2, m2) - getMinutesToday(h1, m1);
+        return `相差${Math.floor(diff/60)}时${diff%60}分`
+    } else {
+        const diff = getMinutesToday(24, 0) - getMinutesToday(h1, m1) + getMinutesToday(h2, m2);
+        return `相差${Math.floor(diff/60)}时${diff%60}分`
+    }
+}
+// 时间点转换成分钟
+function getMinutesToday(h, m) {
+    return h * 60 + m
+}
+console.log(getDiff(23,30,01,10));
